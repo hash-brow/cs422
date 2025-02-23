88d22262bfc0107be2abb6a5f570a14a3f81119f
hash-brow <div4804@gmail.com>
2025-02-23 11:11:38 +0000

    Pure C results

---
+++ HW1/C/main/cmp.sh
@@ -0,0 +1,1 @@
+make PIN_ROOT="/home/ubuntu/pin" TARGET=ia32
--- HW1/C/main/main.cpp
+++ HW1/C/main/main.cpp
@@ -3,27 +3,125 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include "pin.H"
+using std::cerr;
+using std::endl;
+using std::ios;
+using std::ofstream;
+using std::string;
 
-FILE* trace;
+ofstream OutFile;
+
+// The running count of instructions is kept here
+// make it static to help the compiler optimize docount
+static UINT64 icount = 0;
+static UINT64 fast_forward_count = 0;
 
 // This function is called before every instruction is executed
-// and prints the IP
-VOID printip(VOID* ip) { fprintf(trace, "%p\n", ip); }
+VOID insCount() { icount++; }
+
+ADDRINT terminate() {
+	return (icount >= fast_forward_count + 1000000000);
+}
+
+ADDRINT fastForward() {
+	return (icount >= fast_forward_count && icount);
+}
+
+std::vector<bool> instructionChunk(1000000000), dataChunk(1000000000);
+
+void exitRoutine() {
+	OutFile.setf(ios::showbase);
+	//TODO
+	// Instruction Memory Footprint
+	// Data Memory Footprint
+
+	UINT32 instruction_access = std::count(instructionChunk.begin(), instructionChunk.end(), 1);
+	UINT32 data_access = std::count(dataChunk.begin(), dataChunk.end(), 1);
+	
+	OutFile << "Instuction Chunks accessed: " << instruction_access << " Footprint: " << instruction_access * 32 << " bytes" << endl;
+	OutFile << "Data Chunks accessed: " << data_access << " Footprint: " << data_access * 32 << " bytes" << endl;
+
+	OutFile.close();
+	
+	exit(0);
+}
+
+void instructionAnalysis(ADDRINT addr, UINT32 size) {
+	// TODO
+	// figure out what arguments are required
+	// mostly should be starting address, and size
+	//
+	
+	// OutFile << (VOID *)addr << " " << size << endl;
+
+	// UINT32 start = static_cast<UINT32>(addr);
+	ADDRINT start = addr >> 5;
+	// UINT32 end = static_cast<UINT32>(addr + size - 1);
+	ADDRINT end = (addr + size - 1) >> 5;
+	
+	// OutFile << "instruction " << start << " " << end << endl;
+
+	for(ADDRINT chunk = start; chunk <= end; chunk++)
+		instructionChunk[chunk] = 1;
+}
+
+void dataAnalysis(ADDRINT addr, UINT32 size) {
+	// TODO
+	// starting address and size
+	
+	// OutFile << (VOID *)addr << " " << size << endl;
+	
+	// UINT32 start = static_cast<UINT32>(addr);
+	ADDRINT start = addr >> 5;
+	// UINT32 end = static_cast<UINT32>(addr + size - 1);
+	ADDRINT end = (addr + size - 1) >> 5;
+		
+	// OutFile << "data " << start << " " << end << endl;
+
+	for(ADDRINT chunk = start; chunk <= end; chunk++)
+		dataChunk[chunk] = 1;
+}
 
 // Pin calls this function every time a new instruction is encountered
 VOID Instruction(INS ins, VOID* v)
 {
-    // Insert a call to printip before every instruction, and pass it the IP
-    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)printip, IARG_INST_PTR, IARG_END);
+	INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)insCount, IARG_END);
+	INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)terminate, IARG_END);
+	INS_InsertThenCall(ins, IPOINT_BEFORE, (AFUNPTR)exitRoutine, IARG_END);
+	INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)fastForward, IARG_END);
+	INS_InsertThenCall(ins, IPOINT_BEFORE, (AFUNPTR)instructionAnalysis, IARG_INST_PTR, IARG_UINT32, INS_Size(ins), IARG_END);	
+	// INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)fastForward, IARG_END);
+	// INS_InsertThenPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)dataAnalysis, ..., IARG_END);
+	
+	UINT32 memOps = INS_MemoryOperandCount(ins);
+	for(UINT32 memOp = 0; memOp < memOps; memOp++) {
+		if(INS_MemoryOperandIsRead(ins, memOp) || INS_MemoryOperandIsWritten(ins, memOp)){
+			INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)fastForward, IARG_END);
+			INS_InsertThenPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)dataAnalysis, IARG_MEMORYOP_EA, memOp, IARG_MEMORYOP_SIZE, memOp, IARG_END);
+		}
+	}
 }
+
+
+KNOB< string > KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "inscount.out", "specify output file name");
+KNOB< string > KnobFastForward(KNOB_MODE_WRITEONCE, "pintool", "f", "0", "Skip over f billion instructions : default (0)");
 
 // This function is called when the application exits
 VOID Fini(INT32 code, VOID* v)
 {
-    fprintf(trace, "#eof\n");
-    fclose(trace);
+    // Write to a file since cout and cerr maybe closed by the application
+    OutFile.setf(ios::showbase);
+    OutFile << "Count " << icount << endl;
+	UINT32 instruction_access = std::count(instructionChunk.begin(), instructionChunk.end(), 1);
+	UINT32 data_access = std::count(dataChunk.begin(), dataChunk.end(), 1);
+	
+	OutFile << "Instuction Chunks accessed: " << instruction_access << " Footprint: " << instruction_access * 32 << " bytes" << endl;
+	OutFile << "Data Chunks accessed: " << data_access << " Footprint: " << data_access * 32 << " bytes" << endl;
+    OutFile.close();
 }
 
 /* ===================================================================== */
@@ -32,20 +130,26 @@ VOID Fini(INT32 code, VOID* v)
 
 INT32 Usage()
 {
-    PIN_ERROR("This Pintool prints the IPs of every instruction executed\n" + KNOB_BASE::StringKnobSummary() + "\n");
+    cerr << "This tool counts the number of dynamic instructions executed" << endl;
+    cerr << endl << KNOB_BASE::StringKnobSummary() << endl;
     return -1;
 }
 
 /* ===================================================================== */
 /* Main                                                                  */
 /* ===================================================================== */
+/*   argc, argv are the entire command line: pin -t <toolname> -- ...    */
+/* ===================================================================== */
 
 int main(int argc, char* argv[])
 {
-    trace = fopen("itrace.out", "w");
-
     // Initialize pin
     if (PIN_Init(argc, argv)) return Usage();
+
+    OutFile.open(KnobOutputFile.Value().c_str());
+    fast_forward_count = (UINT64) std::stoi(KnobFastForward.Value()) * (UINT64)1000000000;
+
+    OutFile << fast_forward_count << endl;
 
     // Register Instruction to be called to instrument instructions
     INS_AddInstrumentFunction(Instruction, 0);
